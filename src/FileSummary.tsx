51e25fc7e267d54354365c19fb4e402bac8d8741
Bram Rooijmans <b.rooijmans@academylabs.nl>
2025-01-08 16:14:29 +0100

    Xenova transformers + spinner

--- src/FileSummary.tsx
+++ src/FileSummary.tsx
@@ -1,13 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Spinner } from '@/components/expansions/spinner';
 import { extractTextFromPDF } from '@/lib/pdfUtils';
+import { env, pipeline, SummarizationPipeline } from '@Xenova/transformers';
+
+env.allowLocalModels = false;
+env.useBrowserCache = true;
 
 interface Props {
   file: File | null;
 }
 
 function FileSummary(props: Props) {
+  const [isSummarizerLoaded, setIsSummarizerLoaded] = useState(false);
   const [summary, setSummary] = useState<string>("");
+  const summarizer = useRef<SummarizationPipeline | null>(null);
+  const model = "Xenova/distilbart-cnn-6-6";
+
+  useEffect(() => {
+    const init = async () => {
+      if (summarizer.current) return;
+      summarizer.current = await pipeline('summarization', model);
+      setIsSummarizerLoaded(true);
+    };
+    init();
+  }, []);
 
   useEffect(() => {
     const fetchSummary = async () => {
@@ -24,9 +41,14 @@ function FileSummary(props: Props) {
         <CardTitle className="text-left">Summary</CardTitle>
       </CardHeader>
       <CardContent>
-        <pre className="p-4 bg-gray-100 rounded-md overflow-auto">
-          <code className="whitespace-pre-wrap break-words">{summary}</code>
-        </pre>
+        {!isSummarizerLoaded &&
+          <Spinner size="medium">Loading {model}...</Spinner>
+        }
+        {isSummarizerLoaded && summary &&
+          <pre className="p-4 bg-gray-100 rounded-md overflow-auto">
+            <code className="whitespace-pre-wrap break-words">{summary}</code>
+          </pre>
+        }
       </CardContent>
     </Card>
   );
