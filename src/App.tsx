d1396857d45363bd912a6641042bfda40661e055
Bram Rooijmans <b.rooijmans@academylabs.nl>
2025-01-10 11:18:14 +0100

    env development/production

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './app.css';
 import FileSummary from './FileSummary';
 import FileUpload from './FileUpload';
 import Navbar from './Navbar';
 
 function App() {
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      const gaId = import.meta.env.VITE_GA_ID;
+      // Dynamically load the Google Analytics script
+      const script = document.createElement("script");
+      script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+      script.async = true;
+      document.head.appendChild(script);
+
+      // Initialize Google Analytics
+      window.dataLayer = window.dataLayer || [];
+      function gtag(...args: any[]) {
+        window.dataLayer.push(args);
+      }
+      gtag("js", new Date());
+      gtag("config", gaId as string);
+    }
+  }, []);
+
   const [uploadedFile, setUploadedFile] = useState<File | null>(null);
 
   return (
